e13b8836f5f9913679da80984a84f9f4525e7592
Athena Siaotong <asiaotong@ucmerced.edu>
2021-12-29 19:06:36 -0800

    Added primitive faq collapsible

--- script.js
+++ script.js
@@ -1,55 +1,14 @@
-import * as THREE from "three";
+var question = document.getElementsByClassName("faq");
+var i;
 
-
-
-// import * as THREE from "three";
-
-// // Scene
-// const scene = new THREE.Scene();
-
-// // Add a cube to the scene
-// const geometry = new THREE.BoxGeometry(3, 1, 3); // width, height, depth
-// const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
-// const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.set(0, 0, 0);
-// scene.add(mesh);
-
-// // Set up lights
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-// scene.add(ambientLight);
-
-// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
-// directionalLight.position.set(10, 20, 0); // x, y, z
-// scene.add(directionalLight);
-
-// // Camera
-// const width = 10;
-// const height = width * (window.innerHeight / window.innerWidth);
-// const camera = new THREE.OrthographicCamera(
-//   width / -2, // left
-//   width / 2, // right
-//   height / 2, // top
-//   height / -2, // bottom
-//   1, // near
-//   100 // far
-// );
-
-// camera.position.set(4, 4, 4);
-// camera.lookAt(0, 0, 0);
-
-// // Renderer
-// const renderer = new THREE.WebGLRenderer({ antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.render(scene, camera);
-
-// // Add it to HTML
-// document.body.appendChild(renderer.domElement);
-
-// const btn = document.querySelector("button"); // Get the button from the page
-// // Detect clicks on the button
-// if (btn) {
-//   btn.onclick = function() {
-//     // The JS works in conjunction with the 'dipped' code in style.css
-//     btn.classList.toggle("dipped");
-//   };
-// }
+for (i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
