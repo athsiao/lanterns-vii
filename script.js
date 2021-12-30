ade4ffc38e13d183a0348f6ff62b0103857b0066
Athena Siaotong <asiaotong@ucmerced.edu>
2021-12-29 21:39:12 -0800

    Added hover effect to tracks, lights on landing page dim slower

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var question = document.getElementsByClassName("faq");
 
 for (var i = 0; i < question.length; i++) {
-  question[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+    question[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
