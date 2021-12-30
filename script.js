601b077e22719301e0ebfb97d688207eb79f600b
Athena Siaotong <asiaotong@ucmerced.edu>
2021-12-29 20:22:58 -0800

    Added bare bones FAQ module

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 var question = document.getElementsByClassName("faq");
-var i;
 
-for (i = 0; i < question.length; i++) {
+for (var i = 0; i < question.length; i++) {
   question[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
     } else {
-      content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
